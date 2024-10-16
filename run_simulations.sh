bc0a27c149a01d64457148c52f871c7712b48a09
eravera <eleonora.ravera@pi.infn.it>
2024-10-16 08:04:55 +0200

    minor

--- run_simulations.sh
+++ run_simulations.sh
@@ -14,7 +14,7 @@ for i in {0..99}
 do
 
   # Imposta il seed uguale all'indice 'i', o un altro calcolo se preferisci
-  seed=$((4100 + i))
+  seed=$((4200 + i))
 
   # Definisce il nome del file di output
   output_file="$output_dir/my_outputfile_${i}.raw" 
