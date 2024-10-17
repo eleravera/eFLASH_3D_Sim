4ba2e97ed1ec10e77904a1b621870b6e5bf222ff
eravera <eleonora.ravera@pi.infn.it>
2024-10-17 21:25:04 +0200

    seed

--- run_simulations.sh
+++ run_simulations.sh
@@ -10,11 +10,11 @@ output_dir="$base_dir/simulations_$timestamp"
 mkdir "$output_dir"
 
 # Ciclo che esegue la simulazione 100 volte
-for i in {0..99}
+for i in {0..199}
 do
 
   # Imposta il seed uguale all'indice 'i', o un altro calcolo se preferisci
-  seed=$((4500 + i))
+  seed=$((4600 + i))
 
   # Definisce il nome del file di output
   output_file="$output_dir/my_outputfile_${i}.raw" 
