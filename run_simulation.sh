7cd97930aa47c25e1b16cd6c264276e162b900b1
eravera <eleonora.ravera@pi.infn.it>
2024-10-24 13:01:52 +0200

    run simulation.sh per lanciare più simulazioni

---
+++ run_simulation.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#Definisci il percorso della sottocartella
+base_dir="./Dose_Map_30mm/spettro_9MeV"
+mkdir -p "$base_dir"  # Crea la cartella base se non esiste
+
+# Crea una cartella con data e ora all'interno della sottocartella
+timestamp=$(date +"%Y%m%d_%H%M%S")
+output_dir="$base_dir/simulations_$timestamp"
+mkdir "$output_dir"
+
+# Ciclo che esegue la simulazione 100 volte
+for i in {0..699}
+do
+
+  # Imposta il seed uguale all'indice 'i', o un altro calcolo se preferisci
+  seed=$((4000 + i))
+
+  # Definisce il nome del file di output
+  output_file="$output_dir/my_outputfile_${i}.raw" 
+  
+  # Comando per lanciare la simulazione
+  #./flash init_vis.mac $seed $output_file >> "$output_dir/log_simulation.log" 2>&1
+  ./flash init_vis.mac $seed $output_file | tee -a "$output_dir/log_simulation.log"
+
+  # Facoltativo: stampa per vedere l'iterazione in corso
+  echo "Eseguito run $i con seed $seed e file di output $output_file"
+done
