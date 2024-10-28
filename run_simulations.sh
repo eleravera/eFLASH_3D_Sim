4e30b6d20b7f61cae4457e1128201202bac27394
eravera <eleonora.ravera@pi.infn.it>
2024-10-28 21:11:31 +0100

    seed 8100, iter 400

--- run_simulations.sh
+++ run_simulations.sh
@@ -10,11 +10,11 @@ output_dir="$base_dir/simulations_$timestamp"
 mkdir "$output_dir"
 
 # Ciclo che esegue la simulazione 100 volte
-for i in {0..99}
+for i in {0..399}
 do
 
   # Imposta il seed uguale all'indice 'i', o un altro calcolo se preferisci
-  seed=$((8000 + i))
+  seed=$((8100 + i))
 
   # Definisce il nome del file di output
   output_file="$output_dir/my_outputfile_${i}.raw" 
