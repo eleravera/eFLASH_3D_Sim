755ff36e153c02c74a72c575367b8e0710c35aef
eravera <eleonora.ravera@pi.infn.it>
2024-10-28 15:47:43 +0100

    seed 8000

--- run_simulations.sh
+++ run_simulations.sh
@@ -14,7 +14,7 @@ for i in {0..499}
 do
 
   # Imposta il seed uguale all'indice 'i', o un altro calcolo se preferisci
-  seed=$((7000 + i))
+  seed=$((8000 + i))
 
   # Definisce il nome del file di output
   output_file="$output_dir/my_outputfile_${i}.raw" 
