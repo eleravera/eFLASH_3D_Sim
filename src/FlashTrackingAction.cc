753fec50278cb1a0c692b9a821d62771d3c4ce3d
eravera <eleonora.ravera@pi.infn.it>
2024-11-14 19:05:08 +0100

    tracking action for checking boundary process

---
+++ src/FlashTrackingAction.cc
@@ -0,0 +1,85 @@
+#include "FlashTrackingAction.hh"
+#include "FlashSteppingAction.hh"
+#include "G4OpticalPhoton.hh"
+#include "G4Track.hh"
+#include "G4SystemOfUnits.hh"
+
+#include <common.hh>
+
+
+FlashTrackingAction::FlashTrackingAction(FlashSteppingAction* steppingAction)
+    : G4UserTrackingAction(), fSteppingAction(steppingAction) {}
+
+FlashTrackingAction::~FlashTrackingAction() {}
+
+
+void FlashTrackingAction::PreUserTrackingAction(const G4Track*){
+
+}
+
+void FlashTrackingAction::PostUserTrackingAction(const G4Track* aTrack){
+
+  const G4ParticleDefinition* particle = aTrack->GetParticleDefinition();
+  
+  if (particle == G4OpticalPhoton::OpticalPhotonDefinition()) {
+  const G4Step* aStep = aTrack->GetStep();
+  G4StepPoint *postStep = aStep->GetPostStepPoint();
+  G4StepPoint *preStep = aStep->GetPreStepPoint();
+  photonProcess::AbsorptionLocation loc;
+  G4ThreeVector genPosition = aTrack->GetVertexPosition();
+  G4ThreeVector momentumDir = aTrack->GetVertexMomentumDirection();
+  G4double theta = momentumDir.angle(G4ThreeVector(0, 1, 0)); // angle with the y-axis
+  G4double phi = std::atan2(momentumDir.x(), momentumDir.z());
+
+  if (postStep->GetPhysicalVolume()) {
+    G4String PostvolumeName = postStep->GetPhysicalVolume()->GetLogicalVolume()->GetName();
+    G4String PreVolumeName = preStep->GetPhysicalVolume()->GetLogicalVolume()->GetName();
+  
+    //G4cout << "Pre-Volume Name: " << PreVolumeName << G4endl;
+    //G4cout << "post-Volume Name: " << PostvolumeName << G4endl;
+
+    if (PostvolumeName == "logicTreatmentRoom"){
+      loc = photonProcess::TREATMENT_ROOM;
+      //G4cout << "loc: " << loc << G4endl;
+    }
+    else if(PostvolumeName == "phantomLog"){
+      loc = photonProcess::PHANTOM;
+      //G4cout << "loc: " << loc << G4endl;
+
+    }
+    else if(PostvolumeName == "pinholeLog"){
+      loc = photonProcess::PINHOLE;
+      //G4cout << "loc: " << loc << G4endl;
+
+    }
+    else if(PostvolumeName == "DetectorLog"){
+      loc == photonProcess::DETECTOR;
+      //G4cout << "loc: " << loc << G4endl;
+
+    }
+    else {
+      loc = photonProcess::OTHER;
+      //G4cout << "loc: " << loc << G4endl;
+
+    }
+
+  } 
+  else if(!(aStep->GetPostStepPoint()->GetPhysicalVolume())) {
+    //G4cout << "out of world" << G4endl;
+    loc = photonProcess::OUT_OF_WORLD;
+  }
+
+  else {
+    G4cout << "WARNING" << G4endl;
+  }
+  photonProcess photon = photonProcess(aTrack->GetTrackID(), genPosition.x()/mm, genPosition.y()/mm, genPosition.z()/mm, theta, phi, fSteppingAction->PhotonTotalInternalReflectionCount, fSteppingAction->PhotonReflectionCount, fSteppingAction->PhotonRefractionCount, loc) ; 
+  photon.print(); 
+  photonProcess_vector.push_back(photon);
+  fSteppingAction->PhotonTotalInternalReflectionCount = 0; 
+  fSteppingAction->PhotonRefractionCount = 0; 
+  fSteppingAction->PhotonReflectionCount = 0; 
+  }
+
+
+
+}
