1a3dffdfbd6af0f0c5f0be07ace509854bcba7de
eravera <eleonora.ravera@pi.infn.it>
2025-03-23 21:57:07 +0100

    minor sim

--- run_simulations.sh
+++ run_simulations.sh
@@ -14,7 +14,7 @@ for i in {0..499}
 do
 
   # Imposta il seed uguale all'indice 'i', o un altro calcolo se preferisci
-  seed=$((5500 + i))
+  seed=$((6000 + i))
 
   # Definisce il nome del file di output
   output_file="$output_dir/d15cm_hole300um_${seed}.raw" 
