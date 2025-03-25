74194ecc508756a69509f824b7be591ffd1cea64
eravera <eleonora.ravera@pi.infn.it>
2025-03-25 11:56:43 +0100

    run 30 simulations hole 300

--- src/FlashDetectorConstruction.cc
+++ src/FlashDetectorConstruction.cc
@@ -281,7 +281,7 @@ std::vector<G4VPhysicalVolume*> FlashDetectorConstruction::ConstructPinhole(G4do
     */
     G4double pinholeThickness = 0.11*mm;
     G4double pinholeSquareSize = fPhantomSizeX + PinholeDistance * 2;
-    G4double innerRadius = 0.400 * mm;
+    G4double innerRadius = 0.300 * mm;
 
     // Geometry
     G4Trd* squareSolid = new G4Trd("BlackSheet", (pinholeSquareSize + pinholeThickness) / 2, 
