d16524d67d94a3db3bfba6696db633ea95d1fc54
eravera <eleonora.ravera@pi.infn.it>
2025-03-25 21:31:54 +0100

    run simulation 100 um

--- run_simulations.sh
+++ run_simulations.sh
@@ -10,15 +10,15 @@ output_dir="$base_dir/simulations_$timestamp"
 mkdir "$output_dir"
 
 # Ciclo che esegue la simulazione 100 volte
-for i in {0..30}
+for i in {0..50}
 do
 
   # Imposta il seed uguale all'indice 'i', o un altro calcolo se preferisci
   seed=$((5000 + i))
 
   # Definisce il nome del file di output
-  output_file="$output_dir/d15cm_hole200um_${seed}.raw" 
-  log_file="$output_dir/d15cm_log_hole200um_${seed}.log"
+  output_file="$output_dir/d15cm_hole100um_${seed}.raw" 
+  log_file="$output_dir/d15cm_log_hole100um_${seed}.log"
   
   # Comando per lanciare la simulazione
   ./flash run.mac $seed $output_file | tee "$log_file"
