0b5f1a099af1ecd7d814d42ece245d80e30f7724
eravera <eleonora.ravera@pi.infn.it>
2026-01-09 15:20:25 +0100

    seed

--- flash.cc
+++ flash.cc
@@ -64,7 +64,7 @@ int main(int argc, char **argv) {
 
   G4Random::setTheSeed(seed);
   auto *runManager = new G4MTRunManager();
-  G4int nThreads = 3; //235;
+  G4int nThreads = 235;
   runManager->SetNumberOfThreads(nThreads);
  
   runManager->SetUserInitialization(new FlashDetectorConstruction);
