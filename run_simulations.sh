c507fac8a5db1795d01b590a0223b07d9dc3dd83
eravera <eleonora.ravera@pi.infn.it>
2026-01-13 14:57:39 +0100

    turn back to 300 um, 15 cm, for efficiency

--- run_simulations.sh
+++ run_simulations.sh
@@ -17,8 +17,8 @@ do
   seed=$((7000 + i))
 
   # Definisce il nome del file di output
-  output_file="$output_dir/d15cm_hole400um_${seed}.raw" 
-  log_file="$output_dir/d15cm_log_hole400um_${seed}.log"
+  output_file="$output_dir/d15cm_hole300um_${seed}.raw" 
+  log_file="$output_dir/d15cm_log_hole300um_${seed}.log"
   
   # Comando per lanciare la simulazione
   ./flash run.mac $seed $output_file | tee "$log_file"
