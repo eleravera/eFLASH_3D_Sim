7552cea6eeadc6ebdbf1675252aedc029999c3d4
eravera <eleonora.ravera@pi.infn.it>
2026-01-14 08:54:26 +0100

    pinhole inner radius 100 um

--- run_simulations.sh
+++ run_simulations.sh
@@ -10,15 +10,15 @@ output_dir="$base_dir/simulations_$timestamp"
 mkdir "$output_dir"
 
 # Ciclo che esegue la simulazione 100 volte
-for i in {0..10}
+for i in {0..20}
 do
 
   # Imposta il seed uguale all'indice 'i', o un altro calcolo se preferisci
   seed=$((7000 + i))
 
   # Definisce il nome del file di output
-  output_file="$output_dir/d15cm_hole150um_${seed}.raw" 
-  log_file="$output_dir/d15cm_log_hole150um_${seed}.log"
+  output_file="$output_dir/d15cm_hole100um_${seed}.raw" 
+  log_file="$output_dir/d15cm_log_hole100um_${seed}.log"
   
   # Comando per lanciare la simulazione
   ./flash run.mac $seed $output_file | tee "$log_file"
