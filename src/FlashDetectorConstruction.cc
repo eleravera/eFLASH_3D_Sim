64b4828b2541cb19542b8cb06c47c6e15c2b2a11
eravera <eleonora.ravera@pi.infn.it>
2026-01-15 01:42:37 +0100

    pinhole inner radius 500 um

--- src/FlashDetectorConstruction.cc
+++ src/FlashDetectorConstruction.cc
@@ -338,7 +338,7 @@ std::vector<G4VPhysicalVolume*> FlashDetectorConstruction::ConstructPinhole(G4do
     */
     G4double pinholeThickness = 0.11*mm;
     G4double pinholeSquareSize = fPhantomSizeX + PinholeDistance * 2;
-    G4double innerRadius = 0.450 * mm;
+    G4double innerRadius = 0.500 * mm;
 
     // Geometry
     G4Trd* squareSolid = new G4Trd("BlackSheet", (pinholeSquareSize + pinholeThickness) / 2, 
