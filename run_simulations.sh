4e88b66002fcae918fa51be4a9c4d7d1522129bc
eravera <eleonora.ravera@pi.infn.it>
2026-03-10 10:36:33 +0100

    seed 10100

--- run_simulations.sh
+++ run_simulations.sh
@@ -10,11 +10,11 @@ output_dir="$base_dir/simulations_$timestamp"
 mkdir "$output_dir"
 
 # Ciclo che esegue la simulazione 100 volte
-for i in {0..100}
+for i in {0..899}
 do
 
   # Imposta il seed uguale all'indice 'i', o un altro calcolo se preferisci
-  seed=$((10000 + i))
+  seed=$((10101 + i))
 
   # Definisce il nome del file di output
   #output_file="$output_dir/d15cm_hole300um_${seed}.raw" 
